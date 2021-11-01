13d826312240c7e81512d20c6e84855181b16768
Tani Kaye <1tanikaye@gmail.com>
2021-11-01 10:42:19 -0400

    commit tree 6c7568d39cd9f55148561f8464fe27fed5878883

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,17 @@
 import React from "react";
+import About from './About.js'
+import Home from './Home.js'
+import NavBar from './NavBar.js'
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
